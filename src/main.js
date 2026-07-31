6567faf701bbdeb2359533a7c3d64ee53ba5bf98
valeriy <portmone1@gmail.com>
2026-07-31 20:05:58 +0300

    feat: add animated Pig King character

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import './js/partials/contact-us-form.js';
 import './js/partials/swiper.js';
 import './js/partials/accordion.js';
 import './js/partials/header-links-observer.js';
+import './js/partials/pig-king.js';
